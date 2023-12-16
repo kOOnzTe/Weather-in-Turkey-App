ff74bbd0e6ff5ea2319c6a5ab402600849b29dbd
Alper Celik <alperwithsteel@gmail.com>
2023-12-16 17:42:39 +0300

    on MainActivity: long-press and double-tap gestures added

--- app/src/main/java/com/mid/bilweatherapp/MainActivity.kt
+++ app/src/main/java/com/mid/bilweatherapp/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.mid.bilweatherapp
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.mid.bilweatherapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(),  DailyForecastRecyclerViewAdapter.RecyclerAdapterInterface {
+class MainActivity : AppCompatActivity(), DailyForecastRecyclerViewAdapter.RecyclerAdapterInterface {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var layoutManager: LinearLayoutManager
     private lateinit var adapter: DailyForecastRecyclerViewAdapter
+    private var gestureDetector: GestureDetectorCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        // Hiding title bar
+        supportActionBar?.hide()
         setContentView(binding.root)
+
+        // Hiding the status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         // Prepare the data for the RecyclerView
         DailyForecastSys.prepareData()
@@ -27,9 +39,31 @@ class MainActivity : AppCompatActivity(),  DailyForecastRecyclerViewAdapter.Recy
         // Fill the RecyclerView with the adapter
         adapter = DailyForecastRecyclerViewAdapter(this, DailyForecastSys.dailyForecasts)
         binding.recyclerSocial.adapter = adapter
+
+        // Combined GestureDetector for both "long-press" and "double-tap" Gestures
+        gestureDetector = GestureDetectorCompat(this, CustomGesture())
+
+        // Set onTouchListener to handle "long-press" and "double-tap"  gestures
+        binding.root.setOnTouchListener { _, event ->
+            gestureDetector?.onTouchEvent(event)
+            true
+        }
     }
 
     override fun displayItem(weather: DailyForecast) {
 
     }
+
+    inner class CustomGesture : GestureDetector.SimpleOnGestureListener() {
+        override fun onDoubleTap(e: MotionEvent): Boolean { // due to prevent 2 times Snackbar, we used onDoubleTap instead of onDoubleTapEvent
+            Snackbar.make(binding.root, "Double tap gesture detected on the weather app!", Snackbar.LENGTH_SHORT).show()
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Snackbar.make(binding.root, "Long press gesture detected on the weather app!", Snackbar.LENGTH_SHORT).show()
+        }
+    }
 }
+
+
